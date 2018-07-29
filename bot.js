06a7211750dbcbdb9b09dfb84c3d68d786c6083e
Aaron Liu <adlccl@rit.edu>
2018-07-28 21:36:24 -0400

    Added a leave functionality, cleaned up the switch statement and added a functionality to ping the owner of the guild when users join

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const auth = require('./auth.json');
+const prefix = auth.prefix;
 
 const bot = new Discord.Client({disableEveryone: true});
 let status = 'Status: ';
@@ -20,50 +21,41 @@ bot.on('ready', async () => {
 
 bot.on('message', async msg => {
     const channel = msg.channel;
+    const guild = msg.guild;
+    
     let msgArgs = msg.channel.lastMessage.content.split(' ');
     let command = msgArgs[0];
     let option = msgArgs[1];
     let value = msgArgs[2];
     let author = msg.author;
     let username = author.username;
-    let tag = author.tag;
+    let tag = author.tag.trim();
+
+    if (author.bot) return;       
 
     switch (command) {
-        case 'ss~help':
-            if (!option) {
-                provideHelp(channel);
-            }
+        case `${prefix}help`:            
+            provideHelp(channel, option);
             break;
 
-        case 'ss~status':
-            if (!option) {
-                provideStatus(channel);
-                provideDate(channel);
-            }
-            else if (option === 'update') {
-                updateStatus(channel, value);
-            }
-            else if (option === 'date') {
-                date = `Tournament Date: ${value}`;
-                channel.send(`Date was updated to: ${value}`);
-            }
+        case `${prefix}status`:
+            statusUtil(channel, option, value);
             break;
         
-        case 'ss~show':
-            if (!option) {
-                showParticipants(channel);
-            }
+        case `${prefix}show`:
+            showParticipants(channel, option);
+            break;
         
-        case 'ss~join':
-            if (option) {
-                joinShodown(channel, option, author, username, tag);
-            }
+        case `${prefix}join`:
+            joinShowdown(guild, channel, option, username, tag, author);
             break;
-        case 'ss~commands':
-            if (!option) {
-                availableCommands(channel);
-            }
+
+        case `${prefix}leave`:
+            leaveShowdown(channel, option, tag, author);
             break;
+
+        case `${prefix}commands`:
+            availableCommands(channel, option);
     }
 });
 
@@ -73,31 +65,38 @@ bot.login(auth.token);
 /**
  * Provide general help information on joining the SundayShowdown tournament
  * @param {TextChannel | DMChannel | GroupDMChannel} channel
+ * @param {string} option
  */
-function provideHelp(channel) {
-    channel.send(`To join SundayShowdown, please follow the below instructions:
-    1. Start the response with "ss~join" and then a space
-    2. Put your Epic gamer tag after the space
-    3. Enter
+function provideHelp(channel, option) {
+    if (!option) {
+        channel.send(`To join SundayShowdown, please follow the below instructions:
+        1. Start the response with "ss~join" and then a space
+        2. Put your Epic gamer tag after the space
+        3. Enter
+        
+        Ex.
+        ss~join myEpicGamerTag`
+        );
+    } 
+    else {
+        channel.send('Those options are not available at the moment.');
+    }
     
-    Ex.
-    ss~join myEpicGamerTag`
-    );
 }
 
 /**
  * Update the status of the tournament
  * @param {TextChannel | DMChannel | GroupDMChannel} channel 
- * @param {string} msg 
+ * @param {string} value 
  */
-function updateStatus(channel, msg) {
-    if (msg.toLowerCase().trim() === 'no') {
+function updateStatus(channel, value) {
+    if (value.toLowerCase().trim() === 'no') {
         status = 'Status: No';
-        channel.send(`Status was updated to: ${msg}`);
+        channel.send(`Status was updated to: ${value}`);
     }
-    else if (msg.toLowerCase().trim() === 'yes') {
+    else if (value.toLowerCase().trim() === 'yes') {
         status = 'Status: Yes';
-        channel.send(`Status was updated to: ${msg}`);
+        channel.send(`Status was updated to: ${value}`);
     }
     else {
         channel.send('Those options are not available at the moment.');
@@ -107,57 +106,141 @@ function updateStatus(channel, msg) {
 /**
  * Provide the status of the tournament
  * @param {TextChannel | DMChannel | GroupDMChannel} channel 
+ * @param {string} option
+ * @param {string} value
  */
-function provideStatus(channel) {
+function provideStatus(channel, option, value) {
     channel.send(status);
+    
+}
+
+/**
+ * Update the date of the tournament
+ * @param {TextChannel | DMChannel | GroupDMChannel} channel 
+ * @param {string} value 
+ */
+function updateDate(channel, value) {
+    date = `Tournament Date: ${value}`;
+    channel.send(`Date was updated to: ${value}`);
 }
 
 /**
  * Provide the date of the tournament
  * @param {TextChannel | DMChannel | GroupDMChannel} channel 
+ * @param {string} option
+ * @param {string} value
  */
-function provideDate(channel) {
+function provideDate(channel, option, value) {
     channel.send(date);
 }
 
 /**
  * Add participants to the tournament
+ * @param {Guild} guild
  * @param {TextChannel | DMChannel | GroupDMChannel} channel 
  * @param {string} gamerTag 
  * @param {string} username 
- * @param {string} tag 
+ * @param {string} tag
+ * @param {string} author
  */
-function joinShodown(channel, gamerTag, username, tag) {
-        // channel.send(`The username is: ${username} and the tag is: ${tag}.`);
-        // channel.send(`The gamer tag you entered: ${gamerTag}.`);        
+function joinShowdown(guild, channel, gamerTag, username, tag, author) {
+    // channel.send(`The username is: ${username} and the tag is: ${tag}.`);
+    // channel.send(`The gamer tag you entered: ${gamerTag}.`); 
+    if (gamerTag) {       
         if (!participants.has(`${tag}`)) {
             participants.set(`${tag}`, `${gamerTag}`);
-            channel.send('You have been added to the list');
+            guild.owner.send(`${author} has joined.`)
+                .then(message => console.log(`Sent message: ${message.content}`))
+                .catch(console.error);
+            channel.send(`Congratulations ${author}! You have been added to the list`);
         }
         else {
-            channel.send('You have already joined the list.')
-        }    
+            channel.send('You have already joined the list.');
+        }
+    }
+}
+
+/**
+ * Remove oneself from the participants list
+ * @param {TextChannel | DMChannel | GroupDMChannel} channel
+ * @param {string} option 
+ * @param {string} tag 
+ * @param {string} author
+ */
+function leaveShowdown(channel, option, tag, author) {
+    if (!option) {
+        if (participants.has(`${tag}`)) {
+            participants.delete(`${tag}`);
+            channel.send(`We hate to see you go ${author}, but you have now been removed from the tournament.`);
+        }
+        else {
+            channel.send('You have not joined the tournment.');
+        }
+    }
+    else {
+        channel.send('Those options are not available at the moment.');
+    }
 }
 
 /**
  * Show the list of participants
  * @param {TextChannel | DMChannel | GroupDMChannel} channel 
+ * @param {string} option
  */
-function showParticipants(channel) {
-    channel.send('List of Participants');
-    participants.forEach( (value,key) => {
-        channel.send(key + ':' + value + '\n');
-    });
+function showParticipants(channel, option) {
+    if (!option) {
+        channel.send('List of Participants:');
+        participants.forEach( (value,key) => {
+            channel.send(key + ':' + value + '\n');
+        });
+    }
+    else {
+        channel.send('Those options are not available at the moment.');
+    }
 }
 
 /**
  * Show the available commands
  * @param {TextChannel | DMChannel | GroupDMChannel} channel 
+ * @param {string} option
  */
-function availableCommands(channel) {
-    channel.send(`Available commands:
-    ss~status | (update | date) : update = Yes or No, date = (MM/DD/YYY)
-    ss~show : List of Participants 
-    ss~join <string> : Joining Tournament 
-    ss~help : How To Join`);
+function availableCommands(channel, option) {
+    if (!option) {
+        channel.send(`Available commands:
+        ss~status  : Find out the status of the tournament
+        ss~status update <Yes or No> : Update the status of the tournament
+        ss~status date <MM/DD/YYYY> : Update the date of the tournment
+        
+        ss~show : List of Participants 
+
+        ss~join <Gamer Tag> : Joining Tournament 
+        ss~leave : Leave the tournment
+
+        ss~help : Provides information on how to join`);
+    }
+    else {
+        channel.send('Those options are not available at the moment.');
+    }
+}
+
+/**
+ * Utility function that handle status of the tournament
+ * @param {TextChannel | DMChannel | GroupDMChannel} channel 
+ * @param {string} option 
+ * @param {string} value 
+ */
+function statusUtil(channel, option, value) {
+    if (!option) {
+        provideStatus(channel, option);
+        provideDate(channel, option);
+    }
+    else if (option === 'update') {
+        updateStatus(channel, value);
+    }
+    else if (option === 'date') {
+        updateDate(channel, value);
+    }  
+    else {
+        channel.send('Those options are not available at the moment.');
+    }  
 }
